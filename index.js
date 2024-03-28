d74a9cffb1b230d9a0601934ac514e053b77a7a8
sagardhavade <sagardhavade1998@gmail.com>
2024-03-28 17:51:35 +0530

    app use add

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var cors = require("cors");
-app.use(cors("*"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-var nm = require("nodemailer");
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var cors = require('cors')
+app.use(cors('*'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+var nm = require('nodemailer')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-const multer = require('multer');
+const multer = require('multer')
 const upload = multer({
   storage: multer.memoryStorage()
-});
+})
 
 var transporter = nm.createTransport({
-  host: "smtp.gmail.com",
+  host: 'smtp.gmail.com',
   port: 587,
   secure: false,
   auth: {
     // user: "tcpltechsp@gmail.com",
     // pass: "qkuldlknsemrtksg",
-    user: "info@atlasindia.co",
-    pass: "uztckilgvoobwjna",
-  },
-});
+    user: 'info@atlasindia.co',
+    pass: 'uztckilgvoobwjna'
+  }
+})
 
-// app.use("/",(req,res) =>{
-//   res.json({message : 'Starting Server'});
-// });
+app.use('/', (req, res) => {
+  res.json({message: 'Starting Server'})
+})
 
-
-app.post("/sendContactDetail",(req,res) =>{
-  let name = req.body.name;
-  let email = req.body.email;
-  let phone = req.body.phone;
-  let subject = req.body.subject;
-  let message = req.body.message;
-  var options ={
+app.post('/sendContactDetail', (req, res) => {
+  let name = req.body.name
+  let email = req.body.email
+  let phone = req.body.phone
+  let subject = req.body.subject
+  let message = req.body.message
+  var options = {
     // from : "tcpltechsp@gmail.com",
     // to : "tcpltechsp@gmail.com",
-    from : "info@atlasindia.co",
-    to : "info@atlasindia.co",
-    subject : subject || "Enquiry",
+    from: 'info@atlasindia.co',
+    to: 'info@atlasindia.co',
+    subject: subject || 'Enquiry',
     html: `
     <!DOCTYPE html>
 <html lang="en">
@@ -53,67 +52,67 @@ app.post("/sendContactDetail",(req,res) =>{
   <title>Atlas</title>
   <style>
     body {
-      font-family: Arial, sans-serif;
-      line-height: 1.6;
-      margin: 0;
-      padding: 0;
-      background: linear-gradient(to right, #ff758c, #ff7eb3, #b47df7, #819dfc);
+      font-family: Arial, sans-serif
+      line-height: 1.6
+      margin: 0
+      padding: 0
+      background: linear-gradient(to right, #ff758c, #ff7eb3, #b47df7, #819dfc)
     }
 
     .container {
-      max-width: 600px;
-      margin: 20px auto;
-      padding: 20px;
-      background-color: rgba(255, 255, 255, 0.8);
-      border-radius: 10px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      max-width: 600px
+      margin: 20px auto
+      padding: 20px
+      background-color: rgba(255, 255, 255, 0.8)
+      border-radius: 10px
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1)
     }
 
     h1 {
-      margin-bottom: 20px;
-      text-align: center;
-      color: #333;
+      margin-bottom: 20px
+      text-align: center
+      color: #333
     }
 
     .details {
-      padding: 20px;
-      border-radius: 5px;
-      box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
-      background-color:rgba(0, 0, 0, 0.1);
+      padding: 20px
+      border-radius: 5px
+      box-shadow: 0 0 15px rgba(0, 0, 0, 0.1)
+      background-color:rgba(0, 0, 0, 0.1)
     }
 
     .details p {
-      margin-bottom: 10px;
-      color: #333;
+      margin-bottom: 10px
+      color: #333
     }
 
     label {
-      font-weight: bold;
-      color: #6c5b7b;
-      display: block;
-      margin-bottom: 5px;
+      font-weight: bold
+      color: #6c5b7b
+      display: block
+      margin-bottom: 5px
     }
 
     .user-info {
-      margin-bottom: 20px;
+      margin-bottom: 20px
     }
 
     .user-info label {
-      font-size: 18px;
+      font-size: 18px
     }
 
     .user-info p {
-      font-size: 16px;
-      margin: 5px 0;
+      font-size: 16px
+      margin: 5px 0
     }
 
     .attachment {
-      margin-top: 20px;
+      margin-top: 20px
     }
 
     .attachment p {
-      font-weight: bold;
-      color: #333;
+      font-weight: bold
+      color: #333
     }
   </style>
 </head>
@@ -146,35 +145,33 @@ app.post("/sendContactDetail",(req,res) =>{
   </div>
 </body>
 </html>`
-};
-
+  }
 
   transporter.sendMail(options, function (error, info) {
     if (error) {
-      console.log(error);
-      res.status(500).send("Couldn't send the email.");
+      console.log(error)
+      res.status(500).send("Couldn't send the email.")
     } else {
-      console.log("Email sent:", info.response);
-      res.send("Email sent successfully.");
+      console.log('Email sent:', info.response)
+      res.send('Email sent successfully.')
     }
-  });
-});
+  })
+})
 
-app.post("/sendmail", upload.single('cv'), (req, res) => {
-  let email = req.body.email;
-  let name = req.body.name;
-  let message = req.body.message;
-  let phone = req.body.phone;
-  let subject = req.body.subject; // Add this line to get the subject
+app.post('/sendmail', upload.single('cv'), (req, res) => {
+  let email = req.body.email
+  let name = req.body.name
+  let message = req.body.message
+  let phone = req.body.phone
+  let subject = req.body.subject // Add this line to get the subject
   // let cv = req.body.cv; // Add this line to get the cv (file)
-  let cv = req.file;
-  console.log(email, name, message, phone, subject, cv);
+  let cv = req.file
+  console.log(email, name, message, phone, subject, cv)
   var options = {
-    from: "info@atlasindia.co",
-    to: "info@atlasindia.co",
-    subject: subject || "Enquiry", // Subject is optional, set a default if not provided
-    html: 
-        `<!DOCTYPE html>
+    from: 'info@atlasindia.co',
+    to: 'info@atlasindia.co',
+    subject: subject || 'Enquiry', // Subject is optional, set a default if not provided
+    html: `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -182,67 +179,67 @@ app.post("/sendmail", upload.single('cv'), (req, res) => {
   <title>Atlas</title>
   <style>
     body {
-      font-family: Arial, sans-serif;
-      line-height: 1.6;
-      margin: 0;
-      padding: 0;
-      background: linear-gradient(to right, #ff758c, #ff7eb3, #b47df7, #819dfc);
+      font-family: Arial, sans-serif
+      line-height: 1.6
+      margin: 0
+      padding: 0
+      background: linear-gradient(to right, #ff758c, #ff7eb3, #b47df7, #819dfc)
     }
 
     .container {
-      max-width: 600px;
-      margin: 20px auto;
-      padding: 20px;
-      background-color: rgba(255, 255, 255, 0.8);
-      border-radius: 10px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      max-width: 600px
+      margin: 20px auto
+      padding: 20px
+      background-color: rgba(255, 255, 255, 0.8)
+      border-radius: 10px
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1)
     }
 
     h1 {
-      margin-bottom: 20px;
-      text-align: center;
-      color: #333;
+      margin-bottom: 20px
+      text-align: center
+      color: #333
     }
 
     .details {
-      padding: 20px;
-      border-radius: 5px;
-      box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
-      background-color:rgba(0, 0, 0, 0.1);
+      padding: 20px
+      border-radius: 5px
+      box-shadow: 0 0 15px rgba(0, 0, 0, 0.1)
+      background-color:rgba(0, 0, 0, 0.1)
     }
 
     .details p {
-      margin-bottom: 10px;
-      color: #333;
+      margin-bottom: 10px
+      color: #333
     }
 
     label {
-      font-weight: bold;
-      color: #6c5b7b;
-      display: block;
-      margin-bottom: 5px;
+      font-weight: bold
+      color: #6c5b7b
+      display: block
+      margin-bottom: 5px
     }
 
     .user-info {
-      margin-bottom: 20px;
+      margin-bottom: 20px
     }
 
     .user-info label {
-      font-size: 18px;
+      font-size: 18px
     }
 
     .user-info p {
-      font-size: 16px;
-      margin: 5px 0;
+      font-size: 16px
+      margin: 5px 0
     }
 
     .attachment {
-      margin-top: 20px;
+      margin-top: 20px
     }
 
     .attachment p {
-      font-weight: bold;
-      color: #333;
+      font-weight: bold
+      color: #333
     }
   </style>
 </head>
@@ -279,53 +276,50 @@ app.post("/sendmail", upload.single('cv'), (req, res) => {
 </body>
 </html>`
 
+  }
 
-  };
-  
-if (cv) {
-  options.attachments = [{
-    filename: cv.originalname,
-    content: cv.buffer
-  }];
-}
+  if (cv) {
+    options.attachments = [{
+      filename: cv.originalname,
+      content: cv.buffer
+    }]
+  }
 
   transporter.sendMail(options, function (error, info) {
     if (error) {
-      console.log(error);
-      res.status(500).send("Couldn't send the email.");
+      console.log(error)
+      res.status(500).send("Couldn't send the email.")
     } else {
-      console.log("Email sent:", info.response);
-      res.send("Email sent successfully.");
+      console.log('Email sent:', info.response)
+      res.send('Email sent successfully.')
     }
-  });
-});
+  })
+})
 
-app.post("/SubscriptionMail", (req, res) => {
-  
-  let email = req.body.email;
-  
-console.log(email);
+app.post('/SubscriptionMail', (req, res) => {
+
+  let email = req.body.email
+
+  console.log(email)
   let options = {
-    from: "info@atlasindia.co",
-    to: "info@atlasindia.co",
-    subject: "Subscription  Mail",
+    from: 'info@atlasindia.co',
+    to: 'info@atlasindia.co',
+    subject: 'Subscription  Mail',
     html: `
     <h3>
     Email : ${email}<br/>
-    </h3>`,
-  };
+    </h3>`
+  }
   transporter.sendMail(options, function (error, info) {
     if (error) {
-      console.log(error);
-      res.status(500).send("couldn't send");
+      console.log(error)
+      res.status(500).send("couldn't send")
     } else {
-      res.send("sent mail");
+      res.send('sent mail')
     }
-  });
-});
-
-
+  })
+})
 
 app.listen(4000, () => {
-  console.log("started");
-});
+  console.log('started')
+})
